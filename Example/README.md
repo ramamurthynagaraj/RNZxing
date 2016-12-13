9d2499941c69f2de7c309be3ef8bb0e280c9a261
Nagaraj <nagarajr@thoughtworks.com>
2016-12-13 15:33:15 +0530

    Included example for IOS - Nagaraj

---
+++ Example/README.md
@@ -0,0 +1,9 @@
+# react-native-zxing
+
+A react native component to generate barcode.
+
+## Running Example
+    - Run `npm install` and `react-native run-ios`
+
+## License
+*MIT*
---
+++ Example/index.ios.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Zxing from 'Zxing'
+
+export default class Example extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Aztec barcode embedding 'Welcome to React Native!'
+        </Text>
+        <Zxing text="Welcome to React Native!"
+            style={styles.barcode} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  barcode: {
+      height: 200,
+      width: 200,
+      alignSelf: 'center'
+  }
+});
+
+AppRegistry.registerComponent('Example', () => Example);
